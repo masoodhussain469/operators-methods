8f3950d4798569f38233b108d5b5804d64b66722
masoodhussain469 <masoodhussain368@gmail.com>
2021-11-09 16:17:59 +0500

    java operators methods

---
+++ java.js
@@ -0,0 +1,305 @@
+// let name="My name is masood";
+//    alert(name);
+// var number=6;
+// console.log(typeof number);
+// var firstName="Masood"
+// var LastName="Hussain"
+//   var num1=8;
+//  var num2=6;
+//  var greater=num1>num2;
+//   console.log(typeof greater);
+//   var num1=8;
+//  var num2=6;
+//  var greater=num1<num2;
+//   console.log(typeof greater);
+//   var num1=8;
+//  var num2=6;
+//  var smaller=num1<num2;
+//   console.log(typeof smaller);
+//   var num1=8;
+//  var num2=6;
+//  var smaller=num1>num2;
+//   console.log(typeof smaller);
+// console.log(typeof firstName+LastName)
+// let firstName="Masood"
+// let LastName="Hussain"
+// console.log(firstName+LastName)
+//   let num1=4;
+//   let num2=5;
+//     let smaller=num1<num2;
+//   console.log(typeof smaller)
+//   let num1=4;
+//   let num2=5;
+//     let smaller=num1>num2;
+//   console.log(typeof smaller)
+//    let num1=3;
+//    let num2=4;
+//     let addition=num1+num2;
+//       console.log(addition)
+//    let num1=3;
+//    let num2=4;
+//     let addition=num1-num2;
+//       console.log(typeof addition)
+// const num1=8;
+// const num2=6;
+//  const greater=num1>num2;
+//   console.log(typeof greater);
+// const num1=8;
+// const num2=6;
+//  const greater=num1<num2;
+//   console.log(typeof greater);
+//   arr=['Masood',22,true]
+//    console.log(typeof arr);
+//   const num1=8;
+//   const num2=6;
+//   const greater=num1>num2;
+//   console.log(typeof greater);
+//  const firstName="Masood"
+//  const LastName="Hussain"
+//    console.log(typeof firstName+LastName);
+//    let myObj = {
+//       name:"masood",
+//       age:22,
+//       male:true
+//    }
+//    console.log(typeof myObj);
+// let myinformation = {
+//           name:"masood",
+//           age:22,
+//           male:true
+//        }
+//        console.log(typeof myinformation);
+//  let myObj2={
+//      name:'Shahid',
+//      age:50,
+//      Female:false
+//  }
+//  console.log(myObj2)
+
+// 2nd class of javascript
+// variations decalaration
+// number
+// let number=6;
+// console.log(number);
+// Boolean
+// let myObj = {
+//     name:"masood",
+//     age: 22,
+//     male:true
+// }
+// console.log(myObj)
+
+
+// Arthimatic Operators
+// Addition
+// let numOne = 10;
+// let numTwo = 12;
+// let numThree = 13;
+// let numFour = 16;
+
+// let addition = (numOne + numTwo + numThree + numFour)
+// console.log(addition);
+
+// Subraction
+// let numOne = 10;
+// let numTwo = 12;
+// let numThree = 13;
+// let numFour = 16;
+// let Subraction = (numOne - numTwo - numThree - numFour)
+// console.log(Subraction);
+
+// Multipilication
+// let numOne = 10;
+// let numTwo = 12;
+// let numThree = 13;
+// let numFour = 16;
+// let Multipilication = (numOne * numTwo * numThree * numFour)
+// console.log(Multipilication);
+
+// Division
+// let numOne = 10;
+// let numTwo = 12;
+// let numThree = 13;
+// let numFour = 16;
+// let Division = (numOne / numTwo / numThree / numFour)
+// console.log(Division);
+
+// Square
+// let numOne = 10;
+// let numTwo = 12;
+// let numThree = 13;
+// let numFour = 16;
+// let square = numThree**2;
+// console.log(square);
+
+// Remainder
+// let numOne = 10;
+// let numTwo = 12;
+// let numThree = 13;
+// let numFour = 16;
+// let Remainder = (numOne % numThree)
+// console.log(Remainder);
+
+
+// string Methods
+//  charAt
+// let myDetail = "My name is masood hussain,i am from sikanderabad nagar no 2 ";
+// console.log(myDetail);
+// let newMyDetail = myDetail.charAt(15);
+//   At
+// console.log(newMyDetail);
+// let newMyDetail = myDetail.at(12);
+// console.log(newMyDetail);
+
+// concat
+// let firstName = "Masood"
+// let lastName = "Hussain"
+// let newStr = lastName.concat(' ' ,firstName);
+//  console.log(newStr);
+//  let newStr1 = firstName.concat(' ' , lastName)
+//  console.log(newStr1);
+
+// endswith
+// let name = "my name is masood"
+//  let newName = name.endsWith('masood')
+//  console.log(newName);
+
+// includes
+// let name = "my name is masood"
+// let newName = name.includes('for');
+//  console.log(newName);
+
+// indexOf
+// let name = "my name is masood"
+// let newName = name.indexOf('d');
+//  console.log(newName);
+
+// lastIndexOf
+// let name = "my name is masood and my nick name is hussain"
+// let newName = name.lastIndexOf('name');
+//  console.log(newName);
+
+// match
+// let name = "my name is masood and my nick name is hussain"
+// let newName = name.match('name');
+//  console.log(newName);
+
+// Repeat
+// let name = "my name is masood and my nick name is hussain"
+// let newName = name.repeat(5);
+//  console.log(newName);
+
+// Replace
+// let name = "my name is masood and my nick name is hussain"
+// let newName = name.replace('hussain' , 'maqsood');
+//  console.log(newName);
+
+// Slice
+// let name = "my name is masood and my nick name is hussain"
+// let newName = name.slice(5);
+//  console.log(newName);
+
+// let name = "my name is masood and my nick name is hussain"
+// let newName = name.slice(5,17);
+//  console.log(newName);
+
+// small
+// let name = "My name is masood and my nick name is hussain"
+// let newName = name.small();
+//  console.log(newName);
+
+
+// Arrays
+// let myArr = [ 'masood' ,21 ,true ,'Gilgit']
+
+//  console.log( myArr.length)
+//   let newArr = myArr[1]
+//   console.log(newArr);
+
+//  Multidimension Arrays
+//  let myArr = [
+//      ["masood" , 21 , "Nagar"] ,
+//      ["Abid" , 18 , "Hunza"] ,
+//      ["Suhail" , 20, "Astore"]
+//  ]
+//    console.log(myArr.length)
+
+//    let newArr = myArr[0][0]
+//    console.log(newArr);
+
+// nested arrays
+//  let myArr = [
+//      ["masood" , 21 , "Nagar"] ,
+//      ["Abid" , 18 , "Hunza", ["Jamal" ,18 ,"Islamabad"] ] ,
+//      ["Suhail" , 20, "Astore"]
+//  ]
+//    console.log(myArr.length)
+
+//    let newArr = myArr[1][3][2]
+//    console.log(newArr);
+
+// let myDetail = [
+//     {
+//         firstName: "Masood",
+//         lastName: "Hussain",
+//         age: 21,
+//         Address: "Nagar"
+
+//     },
+//     {
+
+//         firstName: "Muhammad",
+//         lastName: "Abbas",
+//         age: 21,
+//         Address: "Gilgit"
+
+//     }
+// ]
+
+// let myIntro = `my name is ${myDetail[0].firstName + " " + myDetail[0].lastName}. I am ${myDetail[0].age} and from ${myDetail[0].Address}`
+// console.log(myIntro);
+// let newIntro = `my name is ${myDetail[1].firstName} , ${myDetail[1] .lastName}`
+// console.log(newIntro)
+
+
+
+
+// Array Methods
+// Example-1
+// pop 
+let myArr = ["masood","sameer" , "Amjad" , "Shahid" , "Anit"]
+
+ console.log(myArr.pop());
+
+ console.log(myArr)
+
+// push
+ let newArr = myArr.push("Jamal")
+ console.log(myArr)
+
+//  shift
+let nArr = myArr.shift()
+console.log(myArr)
+
+// Unshift
+let mArr = myArr.unshift("Saim" , "Muzammil" , "Suhail")
+console.log(myArr)
+
+// Example-2
+// pop
+let myNum = [ 2 , 4 ,  6 , 8 ,10]
+console.log(myNum.pop())
+console.log(myNum)
+
+// push
+let newNum = myNum.push(12 ,14 ,16)
+console.log(myNum)
+
+// shift
+let nNum = myNum.shift()
+    console.log(myNum)
+
+ // Unshift
+let mNum = myNum.unshift(2,4,3,1,)
+console.log(myNum)
+
